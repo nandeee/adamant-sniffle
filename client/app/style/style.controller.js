6c081073c548d6d89f7a580899862b2b286f7142
Nandwana Abhishek <nandwana92@gmail.com>
2015-11-04 23:59:43 +0530

    Added side nav

--- client/app/style/style.controller.js
+++ client/app/style/style.controller.js
@@ -1,6 +1,97 @@
 'use strict';
 
 angular.module('adamantSniffleApp')
-  .controller('StyleCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('StyleCtrl', function($scope) {
+    $scope.sideMenu = {
+      'style': {
+        'one-l1': {
+          'name': 'One L1',
+          'open': false,
+          'leafNode': false,
+          'subMenu': {
+            'one-l2': {
+              'name': 'One L2',
+              'open': false,
+              'leafNode': false,
+              'subMenu': {
+                'one-l3': {
+                  'name': 'One L3',
+                  'open': false,
+                  'leafNode': true,
+                  'subMenu': null
+                }
+              }
+            }
+          }
+        },
+        'two-l1': {
+          'name': 'Two L1',
+          'open': false,
+          'leafNode': false,
+          'subMenu': {
+            'two-l2': {
+              'name': 'Two L2',
+              'open': false,
+              'leafNode': true,
+              'subMenu': {
+                'one-l3': {
+                  'name': 'One L3',
+                  'open': false,
+                  'leafNode': true,
+                  'subMenu': null
+                }
+              }
+            }
+          }
+        },
+        'three-l1': {
+          'name': 'Three L1',
+          'open': false,
+          'leafNode': true,
+          'subMenu': null
+        },
+        'four-l1': {
+          'name': 'Four L1',
+          'open': false,
+          'leafNode': true,
+          'subMenu': null
+        },
+        'five-l1': {
+          'name': 'Five L1',
+          'open': false,
+          'leafNode': true,
+          'subMenu': null
+        },
+        'six-l1': {
+          'name': 'Six L1',
+          'open': false,
+          'leafNode': true,
+          'subMenu': null
+        },
+        'seven-l1': {
+          'name': 'Seven L1',
+          'open': false,
+          'leafNode': false,
+          'subMenu': {
+            'seven-l2': {
+              'name': 'Seven L2',
+              'open': false,
+              'leafNode': true,
+              'subMenu': {
+                'one-l3': {
+                  'name': 'One L3',
+                  'open': false,
+                  'leafNode': true,
+                  'subMenu': null
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+
+    $scope.toggleMenu = function(value) {
+      value.open = !value.open;
+    };
   });
