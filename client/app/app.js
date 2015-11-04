7b3b262c7829e5d932e12a6ed2bae506b9dfba11
Nandwana Abhishek <nandwana@arvindinternet.com>
2015-11-04 18:42:41 +0530

    first commit

---
+++ client/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('adamantSniffleApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
